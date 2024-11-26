2d68ed24ca28258b698138b2a14ea3ad27d45fe0
johnlamcw <johnlamcw2015@gmail.com>
2024-11-26 21:36:19 +0800

    Add files via upload

--- datainfo.py
+++ datainfo.py
@@ -2,15 +2,15 @@ client_version = 0.4
 client_title = 'albion-blackmarket-tool v' + str(client_version)
 
 city_list = [
-    'Lymhurst', 'Bridgewatch', 'Fortsterling', 'Caerleon', 'Martlock',
+    'all','Lymhurst', 'Bridgewatch', 'Fortsterling', 'Caerleon', 'Martlock',
      'Thetford',
 ]
 file_list = [
-    'accessories.txt', 'armor1.txt', 'armor2.txt', 'armor3.txt', 'armor4.txt',
+    '1Mage.txt', '2Melee.txt', '3armor.txt', 'accessories.txt', 'armor1.txt', 'armor2.txt', 'armor3.txt', 'armor4.txt',
     'armor5.txt', 'armor6.txt', 'magic1.txt', 'magic2.txt', 'magic3.txt',
     'magic4.txt', 'melee1.txt', 'melee2.txt', 'melee3.txt', 'melee4.txt',
     'melee5.txt', 'off-hand1.txt', 'off-hand2.txt', 'ranged1.txt',
-    'ranged2.txt', 'custom.txt',
+    'ranged2.txt', 
 ]
 quality_list = [
     'normal',
@@ -19,3 +19,7 @@ quality_list = [
     'excelent',
     'masterpiece',
 ]
+city_color = {
+    'Lymhurst':'green', 'Bridgewatch':'yellow', 'Fortsterling':'white', 'Caerleon':'red', 'Martlock':'blue',
+     'Thetford':'purple',
+}
