bd7b7efba6cb8cabc5674ea20a2c2969029e2045
johnlamcw <johnlamcw2015@gmail.com>
2024-11-26 21:37:19 +0800

    Update datainfo.py

--- datainfo.py
+++ datainfo.py
@@ -6,7 +6,7 @@ city_list = [
      'Thetford',
 ]
 file_list = [
-    '1Mage.txt', '2Melee.txt', '3armor.txt', 'accessories.txt', 'armor1.txt', 'armor2.txt', 'armor3.txt', 'armor4.txt',
+    'accessories.txt', 'armor1.txt', 'armor2.txt', 'armor3.txt', 'armor4.txt',
     'armor5.txt', 'armor6.txt', 'magic1.txt', 'magic2.txt', 'magic3.txt',
     'magic4.txt', 'melee1.txt', 'melee2.txt', 'melee3.txt', 'melee4.txt',
     'melee5.txt', 'off-hand1.txt', 'off-hand2.txt', 'ranged1.txt',
